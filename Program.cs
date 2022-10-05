1c41130049ce1c9497c113ccc1821729860b25da
Tatiana Andreeva <tanysha20072@gmail.com>
2022-10-05 21:06:56 +0300

    Zadacha_1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = 0;
+if (y>5)  // если введенное число больше 5, то возводим в квадрат
+{
+    y=x*x;
+    Console.WriteLine($"Квадрат числа равен: "+ y);
+}
+else   // иначе будем возводить в куб
+{
+    y=x*x*x;
+    Console.WriteLine($"Куб числа равен: "+ y);
+}
