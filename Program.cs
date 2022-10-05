346e7379718152fe8b32910aea01762ab4bf63a1
Tatiana Andreeva <tanysha20072@gmail.com>
2022-10-05 21:38:02 +0300

    Zadacha_4

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Задача 2
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
 Console.Clear();
-Console.WriteLine("Введите число: ");
-int x = Convert.ToInt32(Console.ReadLine());
-int y = 0;
-if (y>5)  // если введенное число больше 5, то возводим в квадрат
+Console.WriteLine("Введите число а:");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b=Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
 {
-    y=x*x;
-    Console.WriteLine($"Квадрат числа равен: "+ y);
+    Console.WriteLine($"Наибольшее число а={a}");
 }
-else   // иначе будем возводить в куб
+else
 {
-    y=x*x*x;
-    Console.WriteLine($"Куб числа равен: "+ y);
+    Console.WriteLine($"Наибольшее число b={b}");
 }
